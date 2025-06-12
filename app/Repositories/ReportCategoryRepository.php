110b1613054c9a932b2bf1e548ca3df6594bcb8e
Al-Fatih Art <restu6005@gmail.com>
2025-06-12 08:29:18 +0800

    add report category management with CRUD functionality

--- app/Repositories/ReportCategoryRepository.php
+++ app/Repositories/ReportCategoryRepository.php
@@ -11,30 +11,34 @@ class ReportCategoryRepository implements ReportCategoryRepositoryInterface
     public function getAllReportCategories()
     {
         // Logic to retrieve all residents
-
+        return ReportCategory::all();
     }
 
     public function getReportCategoryById(int $id)
     {
         // Logic to retrieve a resident by ID
-
+        return ReportCategory::where('id', $id)->first();
     }
 
     public function createReportCategory(array $data)
     {
         // Logic to create a new resident
-
+        return ReportCategory::create($data);
     }
 
     public function updateReportCategory(array $data, int $id)
     {
         // Logic to update an existing resident
+        $reportCategory = $this->getReportCategoryById($id);
 
+        return $reportCategory->update($data);
     }
 
     public function deleteReportCategory(int $id)
     {
         // Logic to delete a resident
+        $reportCategory = $this->getReportCategoryById($id);
 
+        return $reportCategory->delete();
     }
 }
